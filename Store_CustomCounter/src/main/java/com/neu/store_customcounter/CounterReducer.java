b419676122aa9b4f45c918ee3f1a79dc433c495f
Nilank <sharmanilank.com>
2019-05-29 10:01:04 -0400

    Add Reducer & Driver class code

--- Store_CustomCounter/src/main/java/com/neu/store_customcounter/CounterReducer.java
+++ Store_CustomCounter/src/main/java/com/neu/store_customcounter/CounterReducer.java
@@ -1,6 +1,31 @@
 
 package com.neu.store_customcounter;
 
-public class CounterReducer {
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CounterReducer extends Reducer<Text, Text, Text, IntWritable>{
+    
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+        int totalRevenue = 0;
+        Iterator<Text> valuesItr = values.iterator();
+        while(valuesItr.hasNext()){
+            String[] data = valuesItr.next().toString().split(",");
+            
+            int price = Integer.parseInt(data[0]);
+            int sales = Integer.parseInt(data[1]);
+            
+            totalRevenue+= (price*sales);
+            
+        }
+        
+        context.write(key, new IntWritable(totalRevenue));
+        
+        
+    }
     
 }
--- Store_CustomCounter/src/main/java/com/neu/store_customcounter/Driver.java
+++ Store_CustomCounter/src/main/java/com/neu/store_customcounter/Driver.java
@@ -1,14 +1,41 @@
 
 package com.neu.store_customcounter;
 
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 /**
  *
  * @author nilan
  */
 public class Driver {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+       Configuration conf = new Configuration();
+       Job job = new Job(conf, "Counters");
+       job.setJarByClass(Driver.class);
+       
+       job.setMapperClass(CounterMapper.class);
+       job.setReducerClass(CounterReducer.class);
+       
+       job.setMapOutputKeyClass(Text.class);
+       job.setMapOutputValueClass(Text.class);
+       
+       FileInputFormat.addInputPath(job, new Path(args[0]));
+       FileOutputFormat.setOutputPath(job, new Path(args[1]));
+              
+       job.setOutputKeyClass(Text.class);
+       job.setOutputValueClass(IntWritable.class);
+       
+       System.exit(job.waitForCompletion(true) ? 0 : 1); 
     }
     
 }
